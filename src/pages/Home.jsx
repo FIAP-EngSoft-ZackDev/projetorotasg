79624379ee5e4df71957d4b703b31999d298462e
Isac <contatoisacn@gmail.com>
2026-09-10 10:48:32 -0300

    chore: Estrutura inicial do projeto rotas

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+        <h1>Bem-vindo a Página<span>Home</span></h1>
+    </>
+  )
+}
+
+export default Home
---
+++ src/pages/Sobre.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sobre = () => {
+  return (
+    <>
+        <h1>Sobre nós</h1>
+    </>
+  )
+}
+
+export default Sobre
