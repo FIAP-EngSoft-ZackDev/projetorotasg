3305f1c86a7a9ea56f89eb5e571543c4927d6d1d
Isac <contatoisacn@gmail.com>
2026-09-10 11:40:09 -0300

    feat: adicionando icones no projeto

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import {Link} from 'react-router-dom'
+import {Home, HomeIcon, Info, Phone} from 'lucide-react'
 
 const NavBar = () => {
   return (
@@ -8,10 +9,10 @@ const NavBar = () => {
       </div>
 
       <div className='flex space-x-4 items-center'>
-        <Link to="/" className='flex item-center px-3 py-2 font-medium text-white hover:text-blue-950'>Home</Link>
-        <Link to="/sobre" className='flex item-center px-3 py-2 font-medium text-white hover:text-blue-950'>Sobre</Link>
+        <Link to="/" className='flex item-center px-3 py-2 font-medium text-white hover:text-blue-950'><Home size={15} />Home</Link>
+        <Link to="/sobre" className='flex item-center px-3 py-2 font-medium text-white hover:text-blue-950'><Info size={15} />Sobre</Link>
         <Link to="/produto" className='flex item-center px-3 py-2 font-medium text-white hover:text-blue-950'>Produto</Link>
-        <Link to="/contato" className='flex item-center px-3 py-2 font-medium text-white hover:text-blue-950'>Contato</Link>
+        <Link to="/contato" className='flex item-center px-3 py-2 font-medium text-white hover:text-blue-950'><Phone size={15} />Contato</Link>
       </div>
     </nav>
   )
